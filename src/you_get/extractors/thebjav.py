70cc623009bac612336a55da78dc39d58ddf946e
Zhengyang Song <songzy_thu@163.com>
2021-11-16 23:05:49 +0800

    thebjav

---
+++ src/you_get/extractors/thebjav.py
@@ -0,0 +1,6 @@
+# https://thebjav.com/2021/11/13/bjcho%ec%b4%88%ed%95%98-rimha-20211013/
+# class="video-player"
+# class="responsive-player"
+# id="robotlink"
+# //streamtape.com/get_video?id=qrDRvKPLemHzgwr&expires=1637144793&ip=F0EVKRSZKxSHDN&token=vkuzvC0cc5JF
+# https://860718898.tapecontent.net/radosgw/qrDRvKPLemHzgwr/Ct7sMm54kub5Pqd84DoWg-DDK3KBzPQU6M433dVQAJx9eVDHI6sZ8G6WcpVN4uZHYNNdlCm0UakFLouK9OfaEVtaohPa1mDjo_DbxjKyeDst2xW7RMTV4JYV93iSQzbUhpLj5SKMTlS4AGgsNf-YlQ_o9mn-zdONvNT81yOwq3t4-BK3R0FanTt6hNfukn7c7Cxrfn6MGbXX0qB0lVm2D32TMEZq4xrVwFk0US_HYk6TREDvspLiLAqYvPrzVGauhTl35vbtKyQb6Iwy_HiicIC_lwVsH1sN1yPaSg_ExtgO7OSs7-2nUCOcK1w/BJCho%EC%B4%88%ED%95%98-rimha-20211013.mp4.mp4
